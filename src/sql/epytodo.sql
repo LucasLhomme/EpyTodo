ca3cf319780f111743bd6456c8efb4025124e258
Lucas Lhomme <lucas.lhomme@epitech.eu>
2025-04-28 00:14:14 +0200

    Init: the subject Epytodo

---
+++ src/sql/epytodo.sql
@@ -0,0 +1,19 @@
+CREATE TABLE user
+(
+    colonne1 id,
+    colonne2 email,
+    colonne3 password,
+    colonne4 name,
+    colonne5 firstname,
+    colonne6 created_at
+)
+
+CREATE TABLE todo
+(
+    colonne1 id,
+    colonne2 title,
+    colonne3 description,
+    colonne4 created_at,
+    colonne5 due_time,
+    colonne6 user_id
+)
