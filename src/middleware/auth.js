65077e9905501e00a3b7412b9f6c1d4427e86f98
Lucas Lhomme <lucas.lhomme@epitech.eu>
2025-05-09 14:46:10 +0200

    fix: error if token is not valid

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,12 +5,10 @@ module.exports = (req, res, next) => {
        const token = req.headers.authorization.split(' ')[1];
        const decodedToken = jwt.verify(token, process.env.SECRET);  // Utilisez la variable d'environnement
        const userId = decodedToken.id;  // Assurez-vous que cela correspond au format de vos tokens
-       req.auth = {
-           userId: userId
-       };
+       req.auth = {userId: userId};
        next();
        if (token != decodedToken)
-        res.status(401).json({msg: " Token is not valid "})
+        res.status(401).json({msg: "Token is not valid"})
    } catch(error) {
        res.status(401).json({ msg: "No token, authorization denied" });  // Message d'erreur plus générique
    }
