364a5d85f482aef11ad942adb7d163913260bd4e
Lucas Lhomme <lucas.lhomme@epitech.eu>
2025-05-13 16:01:30 +0200

    refactor: change the auth var

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,16 +5,14 @@ module.exports = (req, res, next) => {
     try {
         // Récupérer le token du header Authorization
         const token = req.headers['x-auth-token'] || req.headers.authorization?.split(' ')[1];
-        
         // Vérifier si le token existe
         if (!token) {
             return res.status(401).json({ msg: "No token, authorization denied" });
         }
-        
         // Vérifier la validité du token
         try {
             const decoded = jwt.verify(token, process.env.SECRET);
-            req.auth = decoded; // Stocker les infos de l'utilisateur dans req.auth
+            req.auth = { userId: decoded.userId };
             next(); // Passer au middleware suivant
         } catch (error) {
             return res.status(401).json({ msg: "Token is not valid" });
